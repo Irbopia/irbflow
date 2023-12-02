b9b1f33fbaee60c068d540bd8f44be7d3a27a57b
Travis Lockett <66890141+Irbopia@users.noreply.github.com>
2023-12-01 19:34:44 -0500

    Meta and font

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div>
+  return (
+    <main>
+    <div>
     <UserButton />
-    Home</div>;
+    <h1 className="h1-bold">Hello fam!</h1></div>
+    </main>
+  )
 }
