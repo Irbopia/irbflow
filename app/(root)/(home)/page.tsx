964ccf4a12c4edc895e8e5795cee27eb35fa1762
Travis Lockett <66890141+Irbopia@users.noreply.github.com>
2023-12-04 11:19:58 -0500

    Navbar including mobile navbar ui complete

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main>
     <div>
-    <UserButton />
     <h1 className="h1-bold">Hello fam!</h1></div>
     </main>
   )
