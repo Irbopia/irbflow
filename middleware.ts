351aba8cff38f145fb49ae3f49bd77285e97988c
Travis Lockett <66890141+Irbopia@users.noreply.github.com>
2024-01-31 12:42:23 -0500

    Webhooks

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,7 @@ export default authMiddleware({
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
+  ignoredRoutes: ["/api/webhooks", "/api/chatgpt"],
 });
 
 export const config = {
